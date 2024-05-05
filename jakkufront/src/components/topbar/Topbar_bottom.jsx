bc9c471b5949da950c226ff26bc2e9cf224a927a
Kimd0ng <kdongh0406@gmail.com>
2024-05-05 21:51:21 +0900

    Feat: topbar 이동페이지 column 수정

--- jakkufront/src/components/topbar/Topbar_bottom.jsx
+++ jakkufront/src/components/topbar/Topbar_bottom.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "../../styleCss/Topbar.css";
+import { useNavigate } from "react-router-dom";
 
-function topbar_bottom() {
-    return (
-        <div class="notFix">
-            <div><span>홈</span></div>
-            <div><span>전자 제품</span></div>
-            <div><span>전자 제품</span></div>
-            <div><span>전자 제품</span></div>
-            <div><span>전자 제품</span></div>
-            <div><span>전자 제품</span></div>
-            <div><span>전자 제품</span></div>
-        </div>
-    )
+function topbar_bottom(){
+        // const navigate = useNavigate();
+        // const goCategori = () =>{
+        //     navigate('/categori');
+        // }
+
+        // const goBucket = () => {
+        //     navigate('/bucket');
+        // }
+
+        return (
+            <div class="notFix">
+                <div><span>상품 목록</span></div>
+                <div><span>장바구니</span></div>
+                <div><span>내 구매 내역</span></div>
+            </div>
+        )
+
 }
 
 export default topbar_bottom;
