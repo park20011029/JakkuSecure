49420a15d074e6edb8c4bf418573afde8d296e01
Kimd0ng <kdongh0406@gmail.com>
2024-05-08 22:43:39 +0900

    Feat: Top bar Link, state add

--- jakkufront/src/components/topbar/Topbar_bottom.jsx
+++ jakkufront/src/components/topbar/Topbar_bottom.jsx
@@ -1,25 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { Link, useLocation } from 'react-router-dom';
 import "../../styleCss/Topbar.css";
-import { useNavigate } from "react-router-dom";
+import { useRecoilState } from 'recoil';
+import { pageState } from '../../atoms/atom';
 
-function topbar_bottom(){
-        // const navigate = useNavigate();
-        // const goCategori = () =>{
-        //     navigate('/categori');
-        // }
 
-        // const goBucket = () => {
-        //     navigate('/bucket');
-        // }
+function Topbar_bottom(){
+    const [page, setPage] = useRecoilState(pageState);
 
-        return (
-            <div class="notFix">
-                <div><span>상품 목록</span></div>
-                <div><span>장바구니</span></div>
-                <div><span>내 구매 내역</span></div>
-            </div>
-        )
+    const location = useLocation();
 
+    useEffect(() => {
+        const path = location.pathname;
+
+        if (path === '/categori') {
+            setPage(0);
+        } else if (path === '/bucket') {
+            setPage(1);
+        } else if (path === '/buylist') {
+            setPage(2);
+        }
+    }, [location]);
+
+    return (
+        <div className="notFix">
+            <Link to="/categori">
+                <div><span className={page === 0 ? "clicked" : ""}>상품 목록</span></div>
+            </Link>
+            <Link to="/bucket">
+                <div><span className={page === 1 ? "clicked" : ""}>장바구니</span></div>
+            </Link>
+            <Link to="/buylist">
+                <div><span className={page === 2 ? "clicked" : ""}>내 구매 내역</span></div>
+            </Link>
+        </div>
+    );
 }
 
-export default topbar_bottom;
+export default Topbar_bottom;
