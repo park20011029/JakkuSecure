fe9f74ebf9fd79b5d85b310bb241bb32c626f275
Kimd0ng <kdongh0406@gmail.com>
2024-05-08 22:42:37 +0900

    Feat: Add Bucket section

--- jakkufront/src/pages/Bucket.jsx
+++ jakkufront/src/pages/Bucket.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import ModuleStyle from "../ModuleStyle.module.css";
-import Sort from "../components/categori/Sort";
+import styled from "styled-components";
+import Sort from "../components/Sort";
 import BucketItem from "../components/bucket/BucketItem";
 import Buysection from "../components/bucket/Buysection";
+
+const Maintool = styled.div`
+  display: flex;
+`
+
+const Itemtool = styled.div`
+  width: auto;
+`
+
+const Buytool = styled.div`
+  width: auto;
+  z-index: auto;
+`
 
 function SelectItem() {
     return(
         <div className={ModuleStyle.CategoriPage}>
             <Sort/>
-            <div>
-                <BucketItem/>
-                <Buysection/>
-            </div>
+            <Maintool>
+                <Itemtool>
+                    <BucketItem/>
+                </Itemtool>
+                <Buytool>
+                    <Buysection/>
+                </Buytool>
+            </Maintool>
         </div>
     )
 }
