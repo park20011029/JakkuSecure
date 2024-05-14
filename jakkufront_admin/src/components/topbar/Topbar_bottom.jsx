bcc7c822c7a75bb93982f2da5174bb71eefc1208
Kimd0ng <kdongh0406@gmail.com>
2024-05-14 15:40:37 +0900

    Feat: admin topbar 수정

--- jakkufront_admin/src/components/topbar/Topbar_bottom.jsx
+++ jakkufront_admin/src/components/topbar/Topbar_bottom.jsx
@@ -13,25 +13,20 @@ function Topbar_bottom(){
     useEffect(() => {
         const path = location.pathname;
 
-        if (path === '/categori') {
+        if (path === '/list') {
             setPage(0);
-        } else if (path === '/bucket') {
+        } else if (path === '/pickup') {
             setPage(1);
-        } else if (path === '/buylist') {
-            setPage(2);
         }
     }, [location]);
 
     return (
         <div className="notFix">
-            <Link to="/categori">
+            <Link to="/list">
                 <div><span className={page === 0 ? "clicked" : ""}>상품 목록</span></div>
             </Link>
-            <Link to="/bucket">
-                <div><span className={page === 1 ? "clicked" : ""}>장바구니</span></div>
-            </Link>
-            <Link to="/buylist">
-                <div><span className={page === 2 ? "clicked" : ""}>내 구매 내역</span></div>
+            <Link to="/pickup">
+                <div><span className={page === 1 ? "clicked" : ""}>픽업 관리</span></div>
             </Link>
         </div>
     );
