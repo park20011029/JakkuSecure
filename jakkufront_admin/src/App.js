ecd7c55d1f8ab61ff138ef229c2945e82892b37e
Kimd0ng <kdongh0406@gmail.com>
2024-05-14 15:34:51 +0900

    Feat: admin login page 임시 처리

--- jakkufront_admin/src/App.js
+++ jakkufront_admin/src/App.js
@@ -1,28 +1,48 @@
 import './App.css';
 import Topbar from "./pages/Topbar";
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import SelectItem from "./pages/selectItem";
+import {Routes, Route, BrowserRouter, useLocation} from 'react-router-dom';
+import Itemlist from "./pages/Itemlist";
+/*import Login from "./pages/Login"
+import Signup from "./pages/Signup"*/
 import Footer from "./pages/Footer";
-import Bucket from "./pages/Bucket";
-import Buylist from "./pages/Buylist";
+import PickUp from "./pages/PickUp";
 import { RecoilRoot } from "recoil";
-import Modal from "./components/Usermodal";
+import Modal from "./components/Adminmodal";
 import React from "react";
 
 
-function App() {
-  return (
-      <RecoilRoot>
-          <Modal/>
-        <BrowserRouter>
-          <Topbar/>
-          <Routes>
 
-          </Routes>
-          <Footer/>
-        </BrowserRouter>
-      </RecoilRoot>
-  );
+function App() {
+    return (
+        <RecoilRoot>
+            <Modal/>
+            <BrowserRouter>
+                <AppContent/>
+            </BrowserRouter>
+        </RecoilRoot>
+    );
+}
+function AppContent() {
+    const location = useLocation();
+    const isLoginPage1 = location.pathname === "/";
+    const isLoginPage2 = location.pathname === "/signup";
+
+    return (
+        <>
+            {(!isLoginPage1 && !isLoginPage2) && (
+                <Topbar/>
+            )}
+            <Routes>
+                {/*<Route path="/" element={<Login/>}/>
+                <Route path="/signup" element={<Signup/>}/>*/}
+                <Route path="/list" element={<Itemlist/>}/>
+                <Route path="/pickup" element={<PickUp/>}/>
+            </Routes>
+            {(!isLoginPage1 && !isLoginPage2) && (
+                <Footer/>
+            )}
+        </>
+    );
 }
 
 export default App;
