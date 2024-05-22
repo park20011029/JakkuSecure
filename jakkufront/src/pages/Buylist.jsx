34d494ef02898db2c73758b4ee8de9e3dd2ef0d5
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-05-22 17:27:55 +0900

    feat: 내 구매 내역 페이지 연결

--- jakkufront/src/pages/Buylist.jsx
+++ jakkufront/src/pages/Buylist.jsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, {useEffect} from "react";
 import ModuleStyle from "../ModuleStyle.module.css";
 import styled from "styled-components";
 import Sort from "../components/Sort";
 import BuyItem from "../components/buylist/BuyItem";
+import {useRecoilState} from "recoil";
+import {basketItemsState} from "../atoms/atom";
+import api from "../axios";
 
 const Maintool = styled.div`
 `
 
 function Buylist() {
+
+    const [basketItems, setBasketItems] = useRecoilState(basketItemsState);
+
+
+    const fetchBasketItems = async () => {
+        try {
+
+            const response = await api.get('/customers/history', {
+                params: {
+                    latest: 'desc',
+                    status: 'asc',
+                    page: 0, //여기 이 부분에다가 페이지 번호 넣는거 하셈
+                    size: 5
+                }
+            });
+            console.log(response.data.responseDto.selectOrder);
+            setBasketItems(response.data.responseDto.selectOrder);
+        } catch (error) {
+            console.error("Error fetching basket items:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchBasketItems();
+
+    }, []);
+
     return(
         <div className={ModuleStyle.BuylistPage}>
             <Sort/>
             <Maintool>
-                <BuyItem/>
+                {basketItems.map((item, index) => (
+                    <BuyItem
+                    itemstate={item.orderState}
+                    buydate={item.createAt}
+                    imgsrc={item.imageUrl}
+                    itemname={item.itemName}
+                    amount={item.orderItemTotalAmount}
+                    price={item.totalPrice}
+                />
+                )
+                )}
+                
             </Maintool>
         </div>
     )
