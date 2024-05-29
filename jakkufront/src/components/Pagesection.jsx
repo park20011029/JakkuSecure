cba401735f4491efeefa185b6555d4e52285a026
Kimd0ng <kdongh0406@gmail.com>
2024-05-29 20:36:16 +0900

    feat: pagesection ui 개선

--- jakkufront/src/components/Pagesection.jsx
+++ jakkufront/src/components/Pagesection.jsx
@@ -3,6 +3,34 @@ import styled from 'styled-components';
 import {useRecoilState} from 'recoil';
 import {currentState} from '../atoms/atom';
 import {useNavigate} from "react-router-dom";
+import { TfiArrowCircleLeft, TfiArrowCircleRight } from "react-icons/tfi";
+
+const NumberSection = styled.div`
+  display: flex;
+  width: auto;
+  justify-items: center;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 1vh;
+`
+
+const Arrow_box = styled.div`
+  width: 3vw;
+  height: 3vw;
+`
+
+const Arrow_btn_right = styled(TfiArrowCircleRight)`
+  width: 100%;
+  height: 100%;
+`
+
+const Arrow_btn_left = styled(TfiArrowCircleLeft)`
+  width: 100%;
+  height: 100%;
+`
+const Page_text = styled.p`
+  font-size: 1.3vw;
+`
 
 function Pagesection({ totalPage }) {
     const [currentPage, setCurrentPage] = useRecoilState(currentState);
@@ -26,17 +54,17 @@ function Pagesection({ totalPage }) {
 
     return(
         <>
-            <div>
-                <button onClick={handlePrevPage} disabled={currentPage === 1}>
-                    Previous
-                </button>
-                <button onClick={handleNextPage} disabled={currentPage === totalPage}>
-                    Next
-                </button>
-            </div>
-            <p>
-                Page {currentPage} of {totalPage}
-            </p>
+            <NumberSection>
+                <Arrow_box>
+                    <Arrow_btn_left onClick={handlePrevPage} disabled={currentPage === 1}/>
+                </Arrow_box>
+                <Page_text>
+                    Page {currentPage} of {totalPage}
+                </Page_text>
+                <Arrow_box>
+                    <Arrow_btn_right onClick={handleNextPage} disabled={currentPage === totalPage}/>
+                </Arrow_box>
+            </NumberSection>
         </>
     );
 }
